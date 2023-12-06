b0d0d749c6a9d40068bca1ceef04d3b3849b07f1
Eduardo Lima Lacava <86937690+LacavaSP@users.noreply.github.com>
2023-12-06 20:33:50 -0300

    startup

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+app.use(express.static('public'))
+const porta = 2122
+const host = '195.35.37.40'
+
+app.listen(2122, host,() => {
+    console.log(`SERVIDOR DE GEOLOCALIZACAO INICIADO EM http://${host}:${porta}`)
+})
